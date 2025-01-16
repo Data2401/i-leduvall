e1c77aca38949dd8be9e1ac46c85cd3228c9cd42
Lauren Duvall <laurnelizabethd@gmail.com>
2025-01-16 12:38:12 -0600

    changed from squared to cubed

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -1,8 +1,9 @@
 ## This is a R script 
-## Run this code to make a plot of x and x^2 for the integers from 1 to 10
+## Run this code to make a plot of x and x^3 for the integers from 1 to 10
 
 x <- 1:20
-y <- x^2
+y <- x^3
 
 
-plot(x,y, type = "b", main = "Squared integers")
+plot(x,y, type = "b", main = "Cubed integers")
+
